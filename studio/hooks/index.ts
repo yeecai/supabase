870332a4ad7747c10e107a4d4362ee4b3232dba6
dannykng <danny@supabase.io>
2022-09-16 10:13:18 -0700

    Revert "chore: remove examples"
    This reverts commit 05b96de14238115a9e87e58de705ab8ec08f97e8.

--- studio/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './misc/useStore'
-export * from './misc/withAuth'
-export * from './misc/useOptimisticSqlSnippetCreate'
-export * from './misc/useNotifications'
-export * from './deprecated'
-export * from './queries'
-export * from './ui'
